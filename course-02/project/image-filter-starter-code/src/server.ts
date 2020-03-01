4a8e833a68bc85ecc2983fd191e87c677a087abc
ivan <ivan.quon@gmail.com>
2020-03-01 01:17:57 -0500

    initial commit
    project working initially, base todo completed.

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -28,6 +28,73 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get("/filteredimage:image_url?", async ( req, res ) => {
+    //console.log("query image url is: " + (req.query).toString() + "hi");
+    //console.log("query image url is: " + JSON.stringify(req.query));
+
+    let imgUrl = req.query.image_url;
+
+    const VALID_URL_REG_EX = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/g;
+
+    //check imgUrl is not empty
+    if(!imgUrl) {
+      console.log("URL is empty");
+      return res.status(400)
+                .send('image url is required');
+    }
+    else
+    {
+      // validate imgUrl
+      // compare it against our regex to see if its a valid url
+      if(VALID_URL_REG_EX.exec(imgUrl))
+      {
+        // some logging
+        console.log("Looks like a valid URI");
+        
+        const x = await filterImageFromURL(imgUrl);
+
+        /*return res.status(201).sendFile(x, async (err) =>{
+          console.log(x);
+          console.log("i should clean up files here");
+          let fileArr = [];
+          fileArr.push(x);
+          await sleep(3000);
+          deleteLocalFiles(fileArr);
+      });*/
+        return res.status(201).sendFile(x, async (err) => cleanUp(err, x));
+        // return res.status(200)
+        //         .send("@TODO");
+      }
+      else
+      {
+        console.log('Not a URI');
+        return res.status(400).send("not a valid url");
+      }
+    }
+  });
+
+  // wrapper function so we can put stuff logging or additional 
+  // stuff in here before calling the deleteLocalFiles function
+  async function cleanUp(err: Error, file: string) {
+    console.log(file);
+    let fileArr = [];
+    fileArr.push(file);
+    await sleep(3000);
+    deleteLocalFiles(fileArr);
+  }
+
+  /*#START# COMMENT ME OUT */
+  // extra function to sleep so I can see
+  // the temp file be created in the .tmp folder
+  // then see the deleteLocalFiles command working and
+  // deleting the temp created files
+  async function sleep(ms: number){
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
+  /*#END# COMMENT ME OUT */
+
 
   //! END @TODO1
   
